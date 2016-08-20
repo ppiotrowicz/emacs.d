32582efc0ea9489bb42fc0c5d416f772173368f3
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2016-08-20 22:38:18 +0200

    Coffescript mode

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yafolding which-key vimish-fold use-package twilight-bright-theme smart-mode-line rspec-mode restclient rbenv pcre2el org markdown-mode json-mode hydra gruvbox-theme github-browse-file general evil-visualstar evil-surround evil-nerd-commenter evil-matchit evil-magit evil-leader dumb-jump counsel-projectile bundler bind-map avy auto-highlight-symbol))))
+    (coffee-mode yafolding which-key vimish-fold use-package twilight-bright-theme smart-mode-line rspec-mode restclient rbenv pcre2el org markdown-mode json-mode hydra gruvbox-theme github-browse-file general evil-visualstar evil-surround evil-nerd-commenter evil-matchit evil-magit evil-leader dumb-jump counsel-projectile bundler bind-map avy auto-highlight-symbol))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
