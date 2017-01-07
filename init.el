19351efe4523ad10ee8dcd4cff5a12e5a67b74dc
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2017-01-07 13:48:13 +0100

    Introduce ledger mode

--- init.el
+++ init.el
@@ -543,6 +543,21 @@
 (use-package csv-mode
   :ensure t)
 
+(use-package ledger-mode
+  :ensure t
+  :config
+  (progn
+    (defvar pp/ledger-map (make-sparse-keymap) "Ledger keymap.")
+    (general-define-key
+     :keymaps 'pp/ledger-map
+     "a"  '(ledger-add-transaction     :which-key "add transaction")
+     "="  '(ledger-mode-clean-buffer   :which-key "clean")
+     )
+    (bind-map pp/ledger-map
+      :evil-keys (",")
+      :major-modes (ledger-mode)))
+  :mode (("\\.dat" . ledger-mode)))
+
 ;; ----------------------
 ;; ------ MODELINE ------
 ;; ----------------------
