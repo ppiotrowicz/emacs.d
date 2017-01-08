f4a8675016a609db43b17f5440238655408ac9c9
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2017-01-08 19:36:49 +0100

    Move keybindings and functions to separate file

---
+++ config/pp-funcs.el
@@ -0,0 +1,104 @@
+;; custom functions
+
+(defun pp/highlight-symbol-hydra () "Highlights symbol and begins a search hydra."
+  (interactive)
+  (highlight-symbol)
+  (hydra-search/body))
+
+(defun edit-emacs-config ()
+  "Open emacs config file."
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(defun find-in-project ()
+  "Searches in current project."
+  (interactive)
+  (counsel-ag nil (projectile-project-root)))
+
+(defun find-symbol-at-point ()
+  "Searches for symbol under cursor in current project."
+  (interactive)
+  (counsel-ag (thing-at-point 'symbol) (projectile-project-root)))
+
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+(defun toggle-fullscreen ()
+  "Toggle full screen."
+  (interactive)
+  (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
+
+(defun split-window-right-and-focus ()
+  "Split the window horizontally and focus the new window."
+  (interactive)
+  (split-window-right)
+  (windmove-right))
+
+(defun split-window-below-and-focus ()
+  "Split the window vertically and focus the new window."
+  (interactive)
+  (split-window-below)
+  (windmove-down))
+
+(defun pp/swiper-at-point ()
+  (interactive)
+  (swiper (thing-at-point 'symbol)))
+
+(defun terminal-focus ()
+  (interactive)
+  (do-applescript
+   " do shell script \"open -a iterm\"\n"
+   ))
+
+(defun pp/delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
+(defun pp/rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+(defun pp/copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+(diminish 'undo-tree-mode)
+(diminish 'auto-revert-mode)
+
+(setq gc-cons-threshold 800000)
+
+(fset 'ruby_toggle_symbol
+      (lambda (&optional arg)
+        "Keyboard macro for changing :symbol => '' into symbol: ''."
+        (interactive "p")
+        (kmacro-exec-ring-item (quote ([66 120 69 97 58 kp-delete kp-delete kp-delete escape] 0 "%d")) arg)))
+
+(provide 'pp-funcs)
---
+++ config/pp-keybindings.el
@@ -0,0 +1,82 @@
+;; Keybindings config
+
+(general-define-key
+ :states '(normal visual insert emacs)
+ :prefix "SPC"
+ :non-normal-prefix "M-SPC"
+ "'"   '(terminal-focus                      :which-key "iTerm")
+ ":"   '(execute-extended-command            :which-key "M-x")
+ "SPC" '(avy-goto-char                       :which-key "avy char")
+ "a"   '(org-agenda                          :which-key "agenda")
+ "t"   '(neotree-projectile-find             :which-key "neotree find")
+ "T"   '(neotree-projectile                  :which-key "neotree")
+ ;; Buffers
+ "b"   '(:ignore t                           :which-key "buffers")
+ "bb"  '(ivy-switch-buffer                   :which-key "switch buffer")
+ "bd"  '(kill-this-buffer                    :which-key "kill buffer")
+ "bn"  '(evil-buffer-new                     :which-key "new buffer")
+ "TAB" '(switch-to-previous-buffer           :which-key "previous buffer")
+ ;; Help
+ "h"   '(:ignore t                           :which-key "help")
+ "hc"  '(edit-emacs-config                   :which-key "edit config")
+ "hf"  '(describe-function                   :which-key "describe function")
+ "hm"  '(describe-mode                       :which-key "describe mode")
+ "hp"  '(paradox-list-packages               :which-key "paradox")
+ "hv"  '(describe-variable                   :which-key "describe variable")
+ ;; Files
+ "f"   '(:ignore t                           :which-key "files")
+ "fd"  '(pp/delete-file-and-buffer           :which-key "delete file")
+ "ff"  '(counsel-find-file                   :which-key "find file")
+ "fr"  '(pp/rename-file-and-buffer           :which-key "rename file")
+ ;; Git
+ "g"   '(:ignore t                           :which-key "magit")
+ "gs"  '(magit-status                        :which-key "status")
+ "gb"  '(magit-blame                         :which-key "blame")
+ "go"  '(github-browse-file                  :which-key "github browse")
+ ;; Open
+ "o"   '(:ignore t                           :which-key "open")
+ "oc"  '(org-capture                         :which-key "org capture")
+ "ob"  '((lambda () (interactive) (find-file "~/org/bookmarks.org"))  :which-key "bookmarks")
+ "oe"  '((lambda () (interactive) (find-file "~/org/emacs.org"))      :which-key "emacs tasks")
+ "oh"  '((lambda () (interactive) (find-file "~/org/home.org"))       :which-key "home tasks")
+ "ot"  '((lambda () (interactive) (find-file "~/org/today.org"))      :which-key "today tasks")
+ "ol"  '((lambda () (interactive) (find-file "~/org/til.org"))        :which-key "today I learned")
+ "ow"  '((lambda () (interactive) (find-file "~/org/work.org"))       :which-key "work tasks")
+ "of"  '((lambda () (interactive) (find-file "~/finance/ledger.dat")) :which-key "ledger")
+ ;; Project
+ "p"   '(:ignore t                           :which-key "project")
+ "pp"  '(counsel-projectile-switch-project   :which-key "switch project")
+ "pf"  '(counsel-projectile-find-file        :which-key "find file")
+ "pb"  '(counsel-projectile-switch-to-buffer :which-key "find buffer")
+ "pk"  '(projectile-kill-buffers             :which-key "kill buffers")
+ "p/"  '(find-in-project                     :which-key "find string")
+ "/"   '(find-in-project                     :which-key "find string")
+ ;; Windows
+ "w"   '(:ignore t                           :which-key "windows")
+ "ws"  '(split-window-vertically             :which-key "split -")
+ "wS"  '(split-window-below-and-focus        :which-key "split - and focus")
+ "wv"  '(split-window-horizontally           :which-key "split |")
+ "wV"  '(split-window-right-and-focus        :which-key "split | and focus")
+ "wc"  '(delete-window                       :which-key "delete window")
+ "w="  '(balance-windows                     :which-key "balance windows")
+ "ww"  '(ace-window                          :which-key "ace window")
+ "wf"  '(toggle-fullscreen                   :which-key "fullscreen")
+)
+
+;;(define-key ruby-mode-map (kbd "C-c :") 'ruby_toggle_symbol)
+
+(general-nmap "*"   'pp/highlight-symbol-hydra)
+(general-nmap "C-y" 'counsel-yank-pop)
+(general-nmap "gc"  'evilnc-comment-or-uncomment-lines)
+(general-nmap "%"   'evilmi-jump-items)
+;; window movement
+(general-nmap "C-h" 'evil-window-left)
+(general-nmap "C-j" 'evil-window-down)
+(general-nmap "C-k" 'evil-window-up)
+(general-nmap "C-l" 'evil-window-right)
+;; folding
+(general-nmap "zm"  'yafolding-toggle-all)
+(general-nmap "zc"  'yafolding-hide-parent-element)
+(general-nmap "za"  'yafolding-toggle-element)
+
+(provide 'pp-keybindings)
