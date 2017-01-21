c852e42ab9c4dfd3cb801b069ce8ddcf60f7192e
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2017-01-21 21:12:02 +0100

    Ledger reports

--- custom.el
+++ custom.el
@@ -3,9 +3,27 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(ledger-reports
    (quote
-    (org yafolding which-key wgrep use-package speed-type rspec-mode restclient rbenv powerline paradox neotree markdown-mode magithub json-mode highlight-symbol github-browse-file general evil-surround evil-nerd-commenter evil-matchit evil-magit evil-anzu dumb-jump csv-mode crux counsel-projectile company coffee-mode bundler bind-map all-the-icons ace-window))))
+    (("current balance" "ledger -f /Users/ppiotrowicz/finance/ledger.dat bal assets --cleared")
+     (#("expenses monthly" 0 1
+        (idx 0))
+      "ledger -f %(ledger-file) reg expenses -M")
+     (#("incomes monthly" 0 1
+        (idx 1))
+      "ledger -f %(ledger-file) reg incomes -M")
+     (#("bal" 0 1
+        (idx 3))
+      "ledger -f %(ledger-file) bal")
+     (#("reg" 0 1
+        (idx 4))
+      "ledger -f %(ledger-file) reg")
+     (#("payee" 0 1
+        (idx 5))
+      "ledger -f %(ledger-file) payee")
+     (#("account" 0 1
+        (idx 6))
+      "ledger -f %(ledger-file) account")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
