8c1c514a1612a83beb65b24b382f9dd24956f1ce
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2018-06-12 09:51:14 +0200

    Cleanup org theme

--- config/pp-org-mode.el
+++ config/pp-org-mode.el
@@ -25,11 +25,6 @@
     (setq org-src-fontify-natively t)
     (setq org-hide-leading-stars t)
     (setq org-directory "~/org")
-    (setq org-link-abbrev-alist
-          '(("SD"   . "https://getbase.atlassian.net/browse/SD-")
-            ("OOI"  . "https://getbase.atlassian.net/browse/OOI-")
-            ("jira" . "https://getbase.atlassian.net/browse/")
-            ("conf" . "https://getbase.atlassian.net/wiki/display/%h")))
 
     (setq org-log-into-drawer nil)
     (setq org-clock-into-drawer "CLOCKING")
@@ -57,46 +52,46 @@
     (setq org-mobile-directory "~/Dropbox/Aplikacje/MobileOrg")
 
   ;;; Custom fontification
-    (defun +org--tag-face (n)
-      (let ((kwd (match-string n)))
-        (or (and (equal kwd "#") 'org-tag)
-            (and (equal kwd "@") 'org-special-keyword))))
+    ;; (defun +org--tag-face (n)
+    ;;   (let ((kwd (match-string n)))
+    ;;     (or (and (equal kwd "#") 'org-tag)
+    ;;         (and (equal kwd "@") 'org-special-keyword))))
 
-    (defun +org|adjust-faces ()
-      "Correct (and improve) org-mode's font-lock keywords.
+  ;;   (defun +org|adjust-faces ()
+  ;;     "Correct (and improve) org-mode's font-lock keywords.
 
-  1. Re-set `org-todo' & `org-headline-done' faces, to make them respect
-     underlying faces.
-  2. Fontify item bullets
-  3. Fontify item checkboxes (and when they're marked done)"
-      (let ((org-todo (format org-heading-keyword-regexp-format
-                              org-todo-regexp))
-            (org-done (format org-heading-keyword-regexp-format
-                              (concat "\\(?:" (mapconcat #'regexp-quote org-done-keywords "\\|") "\\)"))))
-        (setq
-         org-font-lock-extra-keywords
-         (append (org-delete-all
-                  `(("\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
-                     (0 (org-get-checkbox-statistics-face) t))
-                    (,org-todo (2 (org-get-todo-face 2) t))
-                    (,org-done (2 'org-headline-done t)))
-                  org-font-lock-extra-keywords)
-                 `((,org-todo (2 (org-get-todo-face 2) prepend))
-                   (,org-done (2 'org-headline-done prepend))
-                   ;; Make checkbox statistic cookies respect underlying faces
-                   ("\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
-                    (0 (org-get-checkbox-statistics-face) prepend))
-                   ;; I like how org-mode fontifies checked TODOs and want this to extend to
-                   ;; checked checkbox items:
-                   ("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)"
-                    1 'org-headline-done prepend)
-                   ;; make plain list bullets stand out
-                   ("^ *\\([-+]\\|[0-9]+[).]\\) " 1 'org-list-dt append)
-                   ;; and separators/dividers
-                   ("^ *\\(-----+\\)$" 1 'org-meta-line)
-                   ;; custom #hashtags & @at-tags for another level of organization
-                   ;; TODO refactor this into a single rule
-                   ("\\s-\\(\\([#@]\\)[^ \n]+\\)" 1 (+org--tag-face 2)))))))
+  ;; 1. Re-set `org-todo' & `org-headline-done' faces, to make them respect
+  ;;    underlying faces.
+  ;; 2. Fontify item bullets
+  ;; 3. Fontify item checkboxes (and when they're marked done)"
+  ;;     (let ((org-todo (format org-heading-keyword-regexp-format
+  ;;                             org-todo-regexp))
+  ;;           (org-done (format org-heading-keyword-regexp-format
+  ;;                             (concat "\\(?:" (mapconcat #'regexp-quote org-done-keywords "\\|") "\\)"))))
+  ;;       (setq
+  ;;        org-font-lock-extra-keywords
+  ;;        (append (org-delete-all
+  ;;                 `(("\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
+  ;;                    (0 (org-get-checkbox-statistics-face) t))
+  ;;                   (,org-todo (2 (org-get-todo-face 2) t))
+  ;;                   (,org-done (2 'org-headline-done t)))
+  ;;                 org-font-lock-extra-keywords)
+  ;;                `((,org-todo (2 (org-get-todo-face 2) prepend))
+  ;;                  (,org-done (2 'org-headline-done prepend))
+  ;;                  ;; Make checkbox statistic cookies respect underlying faces
+  ;;                  ("\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
+  ;;                   (0 (org-get-checkbox-statistics-face) prepend))
+  ;;                  ;; I like how org-mode fontifies checked TODOs and want this to extend to
+  ;;                  ;; checked checkbox items:
+  ;;                  ("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)"
+  ;;                   1 'org-headline-done prepend)
+  ;;                  ;; make plain list bullets stand out
+  ;;                  ("^ *\\([-+]\\|[0-9]+[).]\\) " 1 'org-list-dt append)
+  ;;                  ;; and separators/dividers
+  ;;                  ("^ *\\(-----+\\)$" 1 'org-meta-line)
+  ;;                  ;; custom #hashtags & @at-tags for another level of organization
+  ;;                  ;; TODO refactor this into a single rule
+  ;;                  ("\\s-\\(\\([#@]\\)[^ \n]+\\)" 1 (+org--tag-face 2)))))))
 
     (setq org-capture-templates
           (quote
@@ -121,15 +116,15 @@
             )))
 
     ;; fix level 1 heading colors
-    (set-face-attribute 'org-level-1 nil
-                        :background "#262c34"
-                        :foreground "#00B3EF"
-                        :box nil
-                        :height 1.0)
-    (set-face-attribute 'org-level-2 nil :height 1.0)
-    (set-face-attribute 'org-level-3 nil :height 1.0)
-    (set-face-attribute 'org-level-4 nil :height 1.0)
-    (set-face-attribute 'org-level-5 nil :height 1.0)
+    ;; (set-face-attribute 'org-level-1 nil
+    ;;                     :background "#262c34"
+    ;;                     :foreground "#00B3EF"
+    ;;                     :box nil
+    ;;                     :height 1.0)
+    ;; (set-face-attribute 'org-level-2 nil :height 1.0)
+    ;; (set-face-attribute 'org-level-3 nil :height 1.0)
+    ;; (set-face-attribute 'org-level-4 nil :height 1.0)
+    ;; (set-face-attribute 'org-level-5 nil :height 1.0)
 
     ;; Keybindings
     (evil-set-initial-state 'org-agenda-mode 'normal)
