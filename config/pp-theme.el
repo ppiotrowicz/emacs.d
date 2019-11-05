b1d68ce00b608d96d17e58bc57f0e812e7fa173b
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2019-11-05 20:32:06 +0100

    Workaround for modeline bell

--- config/pp-theme.el
+++ config/pp-theme.el
@@ -53,9 +53,34 @@
 (doom-themes-org-config)
 
 (doom-themes-visual-bell-config)
+;; WORKAROUND: use legacy codes
+;; (set-face-attribute 'doom-visual-bell nil
+;;                     :background (face-foreground 'error)
+;;                     :inverse-video nil)
 (set-face-attribute 'doom-visual-bell nil
                     :background "#54252c"
                     :inverse-video nil)
+
+(defvar doom-themes--bell-p nil)
+(defun doom-themes-visual-bell-fn ()
+  "Blink the mode-line red briefly. Set `ring-bell-function' to this to use it."
+  (unless doom-themes--bell-p
+    (let ((old-remap (copy-alist face-remapping-alist)))
+      (setq doom-themes--bell-p t)
+      (setq face-remapping-alist
+            (append (delete (assq 'mode-line face-remapping-alist)
+                            face-remapping-alist)
+                    '((mode-line doom-visual-bell))))
+      (force-mode-line-update)
+      (run-with-timer 0.15 nil
+                      (lambda (remap buf)
+                        (with-current-buffer buf
+                          (when (assq 'mode-line face-remapping-alist)
+                            (setq face-remapping-alist remap
+                                  doom-themes--bell-p nil))
+                          (force-mode-line-update)))
+                      old-remap
+                      (current-buffer)))))
 
 ;;; Solaire - 'real' buffers are brighter
 (use-package solaire-mode
